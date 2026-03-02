0eeeb4cbf7180c9bcac4a5ceecbdcd6c2e825ca1
sdgteam2020 <sdgteam2020@gmail.com>
2026-03-02 16:01:37 +0530

    test: gitleaks sh file is updated

--- gates/gate-0/gitleaks.sh
+++ gates/gate-0/gitleaks.sh
@@ -22,27 +22,32 @@ echo "📄 Using Gitleaks config: $CONFIG_FILE"
 
 # Run Gitleaks inside Docker
 docker run --rm \
+  -u $(id -u):$(id -g) \
   -v "$PWD:/repo" \
   -v "$CONFIG_FILE:/config.toml:ro" \
   zricethezav/gitleaks:latest \
   detect \
-    --source=/repo \
+    --source=/repo/backend \
+    --source=/repo/frontend \
     --no-git \
     --config=/config.toml \
     --redact \
     --report-format=json \
-    --report-path=/repo/$REPORT_FILE
+    --report-path=/repo/$REPORT_FILE || true
 
 # If report exists and is non-empty → check findings
-if [ -s "$REPORT_FILE" ]; then
+
+if [ -f "$REPORT_FILE" ]; then
   COUNT=$(jq length "$REPORT_FILE" 2>/dev/null || echo 0)
 
   if [ "$COUNT" -gt 0 ]; then
     echo "❌ Gitleaks found $COUNT secrets"
+    echo "----------------------------------"
+    jq -r '.[] | "File: \(.File)\nRule: \(.RuleID)\nLine: \(.StartLine)\n"' "$REPORT_FILE"
+    echo "----------------------------------"
     exit 1
   fi
 fi
 
 echo "✅ Gitleaks PASSED – No secrets detected"
 exit 0
-
