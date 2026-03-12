c110ebe3c18ffe07c8ba41f6c67c0b96e30b8c6d
sdgteam2020 <sdgteam2020@gmail.com>
2026-03-12 16:06:52 +0530

    WP#166 gate4_policy sh file is updated to test

--- gates/gate-4/gate4_policy.sh
+++ gates/gate-4/gate4_policy.sh
@@ -36,7 +36,7 @@ echo "⏱️ Average Response Time: $AVG_RESPONSE ms"
 # -------------------------------
 # 2️⃣ Performance SLA Check
 # -------------------------------
-SLA_THRESHOLD=2000
+SLA_THRESHOLD=10
 
 echo "⏱️ Checking SLA (<= ${SLA_THRESHOLD} ms)..."
 
